ea757c0d86cf149cecd77c58f1ab0ff63a925f85
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2016-12-21 14:58:42 +0200

    Added make files

--- make.bat
+++ make.bat
@@ -13,6 +13,8 @@ if NOT "%PAPER%" == "" (
 	set I18NSPHINXOPTS=-D latex_paper_size=%PAPER% %I18NSPHINXOPTS%
 )
 
+set GH_PAGES_SOURCES = source data Makefile make.bat
+
 if "%1" == "" goto help
 
 if "%1" == "help" (
