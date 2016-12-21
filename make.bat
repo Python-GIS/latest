d7218665863fe440be90a8869954eadcc3fddf29
Henrikki Tenkanen <henrikki.tenkanen@helsinki.fi>
2016-12-21 15:41:45 +0200

    Added ending for gh-pages make

--- make.bat
+++ make.bat
@@ -105,6 +105,7 @@ if "%1" == "gh-pages" (
     git commit -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`"
     git push origin gh-pages
     git checkout master
+    goto end
 )
 
 if "%1" == "dirhtml" (
